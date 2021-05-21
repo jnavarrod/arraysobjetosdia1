35cb758d726c337adfacea2052141a644230aadd
Jorge Navarro <jnavarrod@gmail.com>
2021-05-21 02:57:48 -0400

    inicio de repositorio

---
+++ assets/javascript/main.js
@@ -0,0 +1,134 @@
+let arregloDental =[
+    {
+        Especialidad: 'Dental',
+        Horadeatencion: '8:00',
+        medicoEspecialista: 'Verena Ojeda',
+        nombrePaciente: 'David  Esparza Alvarado',
+        RutPaciente : '11371446-8',
+        prevision : 'Isapre'
+
+    },
+    {
+        Especialidad : 'Dental',
+        horadeatencion : '12:00',
+        medicoEspecialista : 'Paulina Poblete Valdivia',
+        nombrePaciente : 'Angelica Rodríguez',
+        RutPaciente : '14257482-9',
+        prevision : 'Isapre'
+    },
+    {
+        Especialidad : 'Dental',
+        horadeatencion : '14:00',
+        medicoEspecialista : 'Yessenia  Vivanco',
+        nombrePaciente : 'Catalina Donoso',
+        RutPaciente : '19763579-7',
+        prevision : 'Fonasa'
+    },
+    {
+        Especialidad : 'Dental',
+        horadeatencion : '17:00',
+        medicoEspecialista : 'Ruddy De Los Reyes',
+        nombrePaciente : 'Rafael Monsalvez',
+        RutPaciente : '20942569-6',
+        prevision : 'Isapre'
+
+    }
+]
+
+let arregloRadiologia =[
+    {
+        Especialidad : 'Radiología',
+        horadeatencion : '8:00',
+        medicoEspecialista : 'Luz  Acuña ',
+        nombrePaciente : 'Maria Millas',
+        RutPaciente : '8255477-7',
+        prevision : 'Fonasa',
+
+    },
+    {
+        Especialidad : 'Radiología',
+        horadeatencion : '8:30',
+        medicoEspecialista : 'Nicole Inostroza',
+        nombrePaciente : 'Betsabé Estefania Urbina Espinosa',
+        RutPaciente : '12180925-7',
+        prevision : 'Isapre'
+    },
+    {
+        Especialidad : 'Radiología',
+        horadeatencion : '9:00',
+        medicoEspecialista : 'Priscilla Sierra',
+        nombrePaciente : 'Camila Avila',
+        RutPaciente : '6175867-4',
+        prevision : 'Fonasa'
+
+    },
+    {
+        Especialidad : 'Radiología',
+        horadeatencion : '10:00',
+        medicoEspecialista : 'Evelin Isabel  Hernández Nuñez ',
+        nombrePaciente : 'Denisse  Valenzuela ',
+        RutPaciente : '13346908-7',
+        prevision : 'Fonasa'
+
+    },
+    {
+        Especialidad : 'Radiología',
+        horadeatencion : '12:00',
+        medicoEspecialista : 'Kimberly Alveal',
+        nombrePaciente : 'Fabiola  Moreno ',
+        RutPaciente : '21413795-k',
+        prevision : 'Fonasa'
+
+    }
+
+
+
+]
+
+let arregloTraumatología=[
+    {
+        Especialidad : 'Traumatología',
+        horadeatencion : '9:00',
+        medicoEspecialista : 'Amanda  Diaz Olivares',
+        nombrePaciente : 'Sophia Duran',
+        RutPaciente : '20678835-6',
+        prevision : 'Isapre'
+    },
+    {
+        Especialidad : 'Traumatología',
+        horadeatencion : '12:00',
+        medicoEspecialista : 'Maria Jose Bastias Solis',
+        nombrePaciente : 'Maria  Fernanda  Ampuero',
+        RutPaciente : '11908245-5',
+        prevision : 'Fonasa',
+
+    },
+    {
+        Especialidad : 'Traumatología',
+        horadeatencion : '16:00',
+        medicoEspecialista : 'Nicole Pinto',
+        nombrePaciente : 'Francisca  Ibañez',
+        RutPaciente : '9188279-5',
+        prevision : 'Fonasa'
+
+    },
+    {
+        Especialidad : 'Traumatología',
+        horadeatencion : '16:30',
+        medicoEspecialista : 'Alondra Allendes Barrera',
+        nombrePaciente : 'Pamela Darrouy Jimenez',
+        RutPaciente : '10551652-5',
+        prevision : 'Fonasa'
+
+    },
+    {
+        Especialidad : 'Traumatología',
+        horadeatencion : '17:00',
+        medicoEspecialista : 'Kamila Cabello',
+        nombrePaciente : 'Saramay Delgado',
+        RutPaciente : '10323105-1',
+        prevision : 'Fonasa'
+
+    }
+
+]
